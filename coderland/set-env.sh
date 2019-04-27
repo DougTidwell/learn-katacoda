40f3620744235d9b7164db99a37b160249c352e8
dougtidwell <doug_tidwell@yahoo.com>
2019-04-27 15:59:35 -0400

    Going back to the OpenShift image.

--- coderland/set-env.sh
+++ coderland/set-env.sh
@@ -1,6 +1,8 @@
 #/usr/local/bin/launch.sh
 #until $(oc get project istio-system &> /dev/null); do sleep 1; done
 echo "in set-env from the github repo"
+oc login -u admin -p admin 
+
 mkdir -p ~/projects/ # && cd ~/projects/
 
 #export PATH=$PATH:/root/installation/istio-1.0.5/bin
